d02fed46017803d7ec8754b1a93c54e79feb0bc6
Niels Sandholt Busch <niels.busch@gmail.com>
2015-09-10 17:59:51 +0200

    update regexp to support non ascii chars

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
     "math"
     "regexp"
     "net/http"
-    "net/url"
     "log"
     "strconv"
     "hash/crc32"
@@ -20,7 +19,7 @@ import (
 
 import "github.com/gographics/imagick/imagick"
 
-var regex, err = regexp.Compile(`/([0-9]+)x([0-9]+)(/[0-9]+)?(/jpg)?/(http[s]?://[\w/\.\-\%_g ]+?)((\.\w+)?)$`)
+var regex, err = regexp.Compile(`/([0-9]+)x([0-9]+)(/[0-9]+)?(/jpg)?/(http[s]?://.+?)((\.\w+)?)$`)
 var cacheDir string
 
 func hash(s string) uint32 {
@@ -174,26 +173,19 @@ func resizeImage(src, dst, outputFormat string, width, height, compression uint)
 }
 
 // get a file using Get and save it in path
-func getAndSaveFile(imageUrl, path string) error {
-    log.Println("calling get on", imageUrl)
-    var Url *url.URL
-    Url, err := url.Parse(imageUrl)
-    if err != nil {
-  		log.Println("Could not parse url", imageUrl)
-      fmt.Println(err)
-    }
-    Url.Path = Url.EscapedPath()
-    resp, err := http.Get(Url.String())
+func getAndSaveFile(url, path string) error {
+    log.Println("calling get on", url)
+    resp, err := http.Get(url)
     log.Println("finished calling get")
     if err != nil {
-        log.Println("Error getting the file", Url.String())
+        log.Println("Error getting the file", url)
         fmt.Println(err)
         return err
     }
     defer resp.Body.Close()
     if code := resp.StatusCode; code != 200 {
         log.Printf("Error getting the file %s: got status code %s",
-                   Url.String(), code)
+                   url, code)
         return err
     }
 
